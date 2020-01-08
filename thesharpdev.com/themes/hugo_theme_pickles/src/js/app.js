ae30b34061c528c03df45eac7df3e4bc56d1489b
William Sharp <wsharp07@gmail.com>
2020-01-07 21:47:32 -0500

    restructure

---
+++ thesharpdev.com/themes/hugo_theme_pickles/src/js/app.js
@@ -0,0 +1,19 @@
+const $ = window.jQuery
+require('zoom.js')
+require('./zoom/transition.js')
+require('slick-carousel')
+
+$(() => {
+  $('#slider').slick({
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        }
+      }
+    ]
+  })
+})
